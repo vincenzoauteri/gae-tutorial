42a9e95e0a6de5364d67cf552715376d7b511104
vincenzoauteri <vincenzo.auteri@hotmail.com>
2014-12-23 16:16:53 +0100

    update resumebuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,84 @@
-var formattedName = "Vincenzo Auteri";
-var myHTMLheaderName = HTMLheaderName.replace("%data%",formattedName);
+var work = {
+  "jobs":[ 
+  { 
+    "employer":"ARCES Research Center", 
+    "title":"Researcher", 
+    "location":"Bologna, Italy",
+    "dates":"2006-2007"
+  },
+  { 
+    "employer":"Intel Corporation", 
+    "title":"Research Intern", 
+    "location":"Santa Clara, CA, USA",
+    "dates":"2007"
+  },
+  { 
+    "employer":"Chevalier Technologies", 
+    "title":"Embedded SW Engineer", 
+    "location":"London, UK",
+    "dates":"2008-2009"
+  },
+  { 
+    "employer":"SEPSA Information Systems", 
+    "title":"Embedded SW Engineer", 
+    "location":"Madrid, ES",
+    "dates":"2009-2012"
+  },
+  { 
+    "employer":"SEPSA Information Systems", 
+      "title":"Technical Project Manager", 
+    "location":"Madrid, ES",
+    "dates":"2009-2012"
+  }
+  ]
+};
 
-var formattedRole= "Software Engineer";
-var myHTMLheaderRole  = HTMLheaderRole.replace("%data%",formattedRole);
+var projects = {};
+var bio = {
+  "name":"Vincenzo Auteri",
+  "role":"SW engineer",
+  "welcomeMessage":"Hello World",
+  "contacts":
+    {
+    "email":"vincenzo.auteri@hotmail.com",
+  "location":"Madrid"
+    }
+};
 
-$("#header").prepend(myHTMLheaderRole);
-$("#header").prepend(myHTMLheaderName);
+var education = 
+{
+  "schools":[
+  {
+    "name":"University of Bologna",
+    "location":"Bologna, Italy",
+    "degree":"Bacherlor's degree in Electronics Engineering",
+    "majors": 
+      [
+      "Electronics",
+    "SW Engineering"
+      ],
+    "dates":"1999-2004",
+    "url":"www.unibo.it"
+},
+  {
+    "name":"University of Bologna",
+    "location":"Bologna, Italy",
+    "degree":"Master's degree in Electronics Engineering",
+    "majors": 
+      [
+      "Electronics",
+    "SW Engineering"
+      ],
+    "dates":"2004-2007",
+    "url":"www.unibo.it"
+  }
+  ],
+    "onlineCourses":
+      [
+      ]
+
+}
+
 
 
 
