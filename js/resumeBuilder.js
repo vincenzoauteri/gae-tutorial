a68243a7e1c02ffed832b5ca54d64f59d30c1c51
Vincenzo Auteri <vincenzo.auteri@hotmail.com>
2014-12-23 16:41:49 -0500

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -37,6 +37,7 @@ var projects = {};
 var bio = {
   "name":"Vincenzo Auteri",
   "role":"SW engineer",
+  "skills":["C/C++ programming","Embedded applications development"],
   "welcomeMessage":"Hello World",
   "contacts":
     {
@@ -80,5 +81,27 @@ var education =
 }
 
 
+$("#main").prepend(HTMLheaderRole.replace("%data%","SW Engineer")); 
+$("#main").prepend(HTMLheaderName.replace("%data%","Vincenzo Auteri")); 
+
+var HTMLheaderRole = '<span>%data%</span><hr/>';
+
+if (bio["skills"]) {
+	console.log ("Object bio has skills object");
+	$("#header").append(HTMLskillsStart);
+	for (var i=0;i<bio.skills.length;i++){
+		console.log ("Skill n. " + i + ": " + bio.skills[i]);
+		var formattedText = HTMLskills.replace("%data%",bio.skills[i]);
+		console.log ("Formatted n. " + i + ": " + formattedText);
+		$("#skills").append(formattedText);
+		console.log ("div n. " + header);
+	}
+	console.log (document.getElementById("header"));
+	console.log (document.getElementsByTagName("h1"));
+	console.log (document.getElementsByTagName("h1").length);
+
+} else {
+	console.log ("Object bio has no skills object");
+}
 
 
